730073241d5be8d517568e113539586b87efe298
Emil Tullstedt <sakjur@gmail.com>
2016-02-04 12:57:35 +0100

    Bag of tasks solution qsort

--- lab1/qsort.c
+++ lab1/qsort.c
@@ -2,54 +2,140 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define MAXELEMS 30
-#define MAXWORKERS 30
+#include <stdbool.h>
+#include <sys/time.h>
+#include <time.h>
+#define MAXELEMS 100000
+#define MAXWORKERS 5
 
 typedef struct {
   int length;
   int* array;
 } arraylist;
 
-void* quicksort(void* arg);
+typedef struct task {
+  pthread_mutex_t lock;
+  arraylist* list;
+  bool sorted;
+  bool partitioned;
+  int pivot;
+  int pequal; // Number equal to the pivot
+  struct task* left;
+  struct task* right;
+} task;
+
+double read_timer();
+task* start_task(arraylist* data);
+void free_task(task*);
+void* worker(void*);
+void sort (task*);
+void partition (task*);
+void merge (task*);
+
 pthread_attr_t attr;
+task* root = NULL;
 
 int main(int argc, char *argv[]) {
+  int n = MAXWORKERS;
+  int elems = MAXELEMS;
   // pthread variables
+  pthread_t workers[n];
 
   // initialize global pthread attributes
   pthread_attr_init(&attr);
   pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
 
-  int elems = MAXELEMS;
   arraylist* data = malloc(sizeof(arraylist));
   data->array = malloc(sizeof(int)*elems);
   data->length = elems;
   for (int i = 0; i < elems; i++) {
-    data->array[i] = random() % 99;
+    data->array[i] = random() % MAXELEMS;
   }
+  root = start_task(data);
 
-  quicksort(data);
-
-  for (int i = 0; i < elems; i++) {
-    printf("%d\n", data->array[i]);
+  double start_time = read_timer();
+  for (int i = 0; i < n; i++) {
+    pthread_create(&workers[i], &attr, worker, (void *) NULL);
   }
+  for (int i = 0; i < n; i++) {
+    pthread_join(workers[i], NULL);
+  }
+  double end_time = read_timer();
+
+  printf("Sorted %i elements using %i worker(s)\n", elems, n);
+  printf("The execution time is %g sec\n", end_time - start_time);
   return 0;
 }
 
-void* quicksort(void* arg) {
-  arraylist* data = (arraylist*) arg;
-  int pivot_index = data->length / 2;
-  int pivot = data->array[pivot_index];
+double read_timer() {
+    static bool initialized = false;
+    static struct timeval start;
+    struct timeval end;
+    if( !initialized )
+    {
+        gettimeofday( &start, NULL );
+        initialized = true;
+    }
+    gettimeofday( &end, NULL );
+    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+task* start_task(arraylist* data) {
+  task* rv = malloc(sizeof(task));
+  pthread_mutex_init(&rv->lock, NULL);
+  rv->sorted = false;
+  rv->partitioned = false;
+  rv->pivot = 0;
+  rv->pequal = 0;
+  rv->left = NULL;
+  rv->right = NULL;
+  rv->list = data;
+  return rv;
+}
+
+void* worker (void* arg) {
+  while(!root->sorted)
+    sort(root);
+  return NULL;
+}
+
+void sort (task* base) {
+  if (pthread_mutex_trylock(&base->lock) == 0) {
+    if (!base->partitioned) {
+      partition(base);
+    }
+
+    if (!base->sorted) {
+      if (base->left->sorted && base->right->sorted) {
+        merge(base);
+      } else {
+        if (!base->left->sorted) {
+          pthread_mutex_unlock(&base->lock);
+          sort(base->left);
+        }
+        if (!base->right->sorted) {
+          pthread_mutex_unlock(&base->lock);
+          sort(base->right);
+        }
+      }
+    }
+  }
+  pthread_mutex_unlock(&base->lock);
+}
+
+void partition (task* base) {
+  arraylist* list = base->list;
+
+  int pivot = list->array[list->length/2];
   int equal = 0;
-  pthread_t lesserworker, greaterworker;
 
   arraylist* lesser = malloc(sizeof(arraylist));
   arraylist* greater = malloc(sizeof(arraylist));
-  lesser->array = malloc(sizeof(int)*data->length);
-  greater->array = malloc(sizeof(int)*data->length);
+  lesser->array = malloc(sizeof(int)*list->length);
+  greater->array = malloc(sizeof(int)*list->length);
 
-  for (int i = 0; i < data->length; i++) {
-    int curr = data->array[i];
+  for (int i = 0; i < list->length; i++) {
+    int curr = list->array[i];
     if (curr < pivot) {
       lesser->array[lesser->length] = curr;
       lesser->length++;
@@ -61,32 +147,49 @@ void* quicksort(void* arg) {
     }
   }
 
-  printf("%d + %d + %d = %d\n", lesser->length, equal, greater->length,
-      data->length);
+  base->pivot = pivot;
+  base->pequal = equal;
+  base->left = start_task(lesser);
+  base->right = start_task(greater);
 
-  if (lesser->length > 1) {
-    pthread_create(&lesserworker, &attr, quicksort, (void *) lesser);
+  if (lesser->length < 2) {
+    base->left->sorted = true;
+    base->left->partitioned = true;
   }
-  if (greater->length > 1) {
-    pthread_create(&greaterworker, &attr, quicksort, (void *) greater);
+  if (greater->length < 2) {
+    base->right->sorted = true;
+    base->right->partitioned = true;
   }
 
-  int greater_offset = lesser->length+equal;
-  if (lesser->length > 1)
-    pthread_join(lesserworker, NULL);
-  memcpy(data->array, lesser->array, lesser->length*sizeof(int));
-  if (greater->length > 1)
-    pthread_join(greaterworker, NULL);
-  memcpy(data->array+greater_offset, greater->array,
-      greater->length*sizeof(int));
+  base->partitioned = true;
+};
+
+void merge (task* base) {
+  task* left = base->left;
+  task* right = base->right;
+  arraylist* list = base->list;
+  int equal = base->pequal;
+
+  if (!left->sorted && !right->sorted)
+    return;
+
+  int* right_pos = list->array + left->list->length + equal;
+  memcpy(list->array, left->list->array, left->list->length*sizeof(int));
+  memcpy(right_pos, right->list->array, right->list->length*sizeof(int));
+
   while (equal > 0) {
-    data->array[lesser->length+equal-1] = pivot;
+    list->array[left->list->length+equal-1] = base->pivot;
     equal--;
   }
-  //free(lesser->array);
-  //free(lesser);
-  //free(greater->array);
-  //free(greater);
 
-  return (void *) data;
+  base->sorted = true;
 }
+
+void free_task (task* base) {
+  if (base) {
+    free(base->list->array);
+    free(base->list);
+    free(base);
+  }
+}
+
