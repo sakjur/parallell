090ee83a8a1f6b335de11224b9d7f6f11c91aa1e
Emil Tullstedt <sakjur@gmail.com>
2016-02-15 17:13:22 +0100

    Added honeybees

---
+++ lab3/honeybees.c
@@ -0,0 +1,107 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define N_BEES 10
+#define JAR_FULL 64
+#define TENTH_SECOND 100000000
+#define HALF_SECOND (5*TENTH_SECOND)
+
+// Global pthread attributes
+pthread_attr_t attr;
+
+sem_t jar_sem;
+sem_t full_sem;
+sem_t empty_sem;
+
+int64_t jar;
+
+void* workerbee(void*);
+void* bearthread(void*);
+
+int main (int argc, char* argv[]) {
+  /*
+   * There are threads for every bird, the baby birds are represented within
+   * an array of pthreads and the parent bird by a pthread directly
+   */
+  pthread_t honeybees[N_BEES];
+  pthread_t bear_id;
+
+  // initialize global pthread attributes
+  pthread_attr_init(&attr);
+  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+
+  /* Initialize synchronization semaphores */
+  sem_init(&jar_sem, 0, 1);
+  sem_init(&full_sem, 0, 0);
+  sem_init(&empty_sem, 0, 0);
+
+  /* Worms in the bowl from the beginning */
+  jar = 0;
+
+  // Start num_workers workers
+  for (int64_t i = 0; i < N_BEES; i++) {
+    pthread_create(&honeybees[i], &attr, workerbee, (void *) i);
+  }
+  pthread_create(&bear_id, &attr, bearthread, NULL);
+
+  pthread_join(bear_id, NULL);
+
+  return 0;
+}
+
+/* The bees who ŕefills the honeyjar one by one */
+void* workerbee(void* arg) {
+  int64_t id = (int64_t) arg;
+  int64_t refillcounter = 0;
+  struct timespec sleeptime = {0};
+
+  /* Fair queueing */
+  struct sched_param param;
+  pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+
+  while(true) {
+    sem_wait(&jar_sem);
+    jar++;
+    refillcounter++;
+    printf("[%ld] Delivered honey for the %ldth time. (%ld/%d)\n",
+        id, refillcounter, jar, JAR_FULL);
+    if (jar >= JAR_FULL) {
+      printf("[%ld] Waking up bear\n", id);
+      sem_post(&full_sem);
+      sem_wait(&empty_sem);
+    }
+    /* Drop-off takes .2 seconds */
+    sleeptime.tv_sec = 0;
+    sleeptime.tv_nsec=2*TENTH_SECOND;
+    nanosleep(&sleeptime, NULL);
+    sem_post(&jar_sem);
+    sleeptime.tv_sec = 2;
+    sleeptime.tv_nsec = 0;
+    nanosleep(&sleeptime, NULL);
+  }
+}
+
+/* The bear which consumes the "honey" */
+void* bearthread(void* arg) {
+  int64_t eatcounter = 1;
+  struct timespec sleeptime = {.tv_sec=0, .tv_nsec=TENTH_SECOND*3};
+  while(true) {
+    sem_wait(&full_sem);
+    printf("[BEAR] Yawn. Waking up\n");
+    while (jar > 0) {
+      jar = jar - 10;
+      if (jar < 0)
+        jar = 0;
+      printf("[BEAR] Eating #%ld (%ld/%d)\n", eatcounter, jar, JAR_FULL);
+      nanosleep(&sleeptime, NULL);
+    }
+    printf("[BEAR] Going to sleep\n");
+    sem_post(&empty_sem);
+    eatcounter++;
+  }
+}
+
